6cd4df36b1c33c1b844682760f8e3338dc49ee80
TupiNUMBooR <TupiNUMBooR@gmail.com>
2023-01-18 01:18:03 +0300

    Init

---
+++ mindbox-test/Tests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace mindbox_test {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
